adf92c1ee62f47549db93a83f8f33a1133aaefee
jonathan-mayer <jonathanmayer2007@gmail.com>
2025-06-30 08:41:19 +0200

    fix: check output of deadcode

--- deadcode.sh
+++ deadcode.sh
@@ -4,4 +4,8 @@ if ! command -v deadcode &>/dev/null; then
     go install golang.org/x/tools/cmd/deadcode@latest
 fi
 
-exec deadcode ./...
+output=$(deadcode ./...)
+if [[ -n "$output" ]]; then
+    echo "$output"
+    exit 1
+fi
